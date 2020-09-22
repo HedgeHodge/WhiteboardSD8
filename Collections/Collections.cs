a02f7f1b7500ad145cc525ba86f25010acc551f3
Tyler Hodges <Hodges3902@gmail.com>
2020-09-22 12:59:51 -0400

    Add collections challenge

---
+++ Collections/Collections.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collections
+{
+    public class Collections
+    {
+        static Random rand = new Random();
+
+        public static int[] TestScores(int numOfScores)
+        {
+            int[] scores = new int[numOfScores];
+            for (int i = 0; i < numOfScores; i++)
+            {
+                scores[i] = rand.Next(101);
+            }
+
+            return scores;
+        }
+
+        public static double AverageScore(int[] scores)
+        {
+            double sum = scores.Sum();
+            return Math.Floor(sum / scores.Length); 
+        }
+
+        public static int GetMaxVal(int[] arrInput)
+        {
+            int max = arrInput[0];
+            for (int i = 0; i < arrInput.Length; i++)
+            {
+                if(arrInput[i] > max)
+                {
+                    max = arrInput[i];
+                }
+            }
+            return max;
+        }
+
+        public static int[] sortArray(int[] arrInput)
+        {
+            for (int i = 0; i < arrInput.Length - 1; i++)
+            {
+                for (int j = 0; j < arrInput.Length - 1; j++)
+                {
+                    if(arrInput[j] > arrInput[j + 1])
+                    {
+                        int temp = arrInput[j];
+                        arrInput[j] = arrInput[j + 1];
+                        arrInput[j+1] = temp;
+                    }
+                }
+            }
+            return arrInput;
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Collections.TestScores(100);
+            Console.WriteLine($"The average is: {Collections.AverageScore(array)}");
+            Console.WriteLine($"The max value is: {Collections.GetMaxVal(array)}");
+            int[] sortedArray = Collections.sortArray(array);
+            foreach(int i in sortedArray)
+            {
+                Console.Write($"{i}, ");
+            }
+        }
+    }
+}
